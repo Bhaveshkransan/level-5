4791e9586949081b7952576f55dc70baebd91aae
Bhavesh Gangurde <bhaveshg1357@gmail.com>
2026-09-12 14:54:04 +0530

    second

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "hello from level 5 cloud deployment!" });
+  return res.status(200).json({ message: "hello from bhavesh v2" });
 });
 
 app.listen(port, () => {
